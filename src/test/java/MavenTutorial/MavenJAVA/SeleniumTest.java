087feafc08d190f1a5a3f53223d2e3fc13366923
AishwaryaShivaram <aish.swthrt@gmail.com>
2021-07-14 11:03:34 +0530

    first commit

---
+++ src/test/java/MavenTutorial/MavenJAVA/SeleniumTest.java
@@ -0,0 +1,18 @@
+package MavenTutorial.MavenJAVA;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomatio() {
+		System.out.println("browserAutomatio");
+	}
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+
+}
